9e29873e3c1b49159a9c82788e56fe9ace0936f7
Dipak Poudel <pdl.dipak@gmail.com>
2021-07-28 23:09:16 +0200

    fixed the header styling

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -1,44 +1,47 @@
-import React from 'react';
-import { Typography, Box, Stack, Container } from '@material-ui/core';
+import * as React from 'react';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 import { styled } from '@material-ui/core/styles';
 import image from '../../assets/images/header01.jpeg';
 
-const Item = styled(Typography)(({ theme }) => ({
-  ...theme.typography.h2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.primary,
-  position: 'absolute',
-  fontSize: '100px',
-  opacity: '1',
-}));
-
-const BlogTitleStack = styled(Stack)`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
-const ImageContainer = styled(Stack)``;
-
-const Image = styled('img')`
-  width: 100%;
-  height: 450px;
-  object-fit: cover;
-`;
-
 const Header: React.FC = () => {
   return (
-    <Box>
-      <BlogTitleStack>
-        <Item>Blog</Item>
-      </BlogTitleStack>
-
-      <ImageContainer>
-        <Image src={image} alt="headerImage" />
-      </ImageContainer>
-    </Box>
+    <BlogTitleStack>
+      <TextBox>
+        <Text>Dipak</Text>
+        <Text>Blog</Text>
+      </TextBox>
+    </BlogTitleStack>
   );
 };
 
 export default Header;
+
+const Text = styled(Typography)(({ theme }) => ({
+  ...theme.typography.h2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.primary.main,
+  fontSize: '100px',
+  fontFamily: 'Lora',
+  fontStyle: 'italic',
+}));
+
+const BlogTitleStack = styled(Paper)({
+  mb: 4,
+  backgroundSize: 'cover',
+  backgroundRepeat: 'no-repeat',
+  backgroundPosition: 'center',
+  backgroundImage: `url(${image})`,
+  width: '100%',
+  height: '450px',
+});
+
+const TextBox = styled(Box)({
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  margin: 'auto',
+});
