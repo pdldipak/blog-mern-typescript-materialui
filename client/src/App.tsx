a5288ed50e83af1f84aaae9bacf7fde2e8e6d459
Dipak Poudel <pdl.dipak@gmail.com>
2021-08-05 22:59:20 +0200

    Changes to make code more clean to read

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,21 +4,30 @@ import { Paper } from '@material-ui/core';
 import TopBar from './components/navbar/TopBar';
 import { Header, MainContainer } from './Styled.App';
 import Navigation from './navigation/Navigation';
+import { BlogsContextProvider } from './context/blogsContext/BlogContext';
+import { CategoryContextProvider } from './context/categoriesContext/CategoriesContext';
+import { UserContextProvider } from './context/authContext/AuthContext';
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
-      <MainContainer>
-        <Header elevation={0}>
-          <TopBar />
-        </Header>
+      <UserContextProvider>
+        <BlogsContextProvider>
+          <CategoryContextProvider>
+            <MainContainer>
+              <Header elevation={0}>
+                <TopBar />
+              </Header>
 
-        <main>
-          <Paper elevation={0}>
-            <Navigation />
-          </Paper>
-        </main>
-      </MainContainer>
+              <main>
+                <Paper elevation={0}>
+                  <Navigation />
+                </Paper>
+              </main>
+            </MainContainer>
+          </CategoryContextProvider>
+        </BlogsContextProvider>
+      </UserContextProvider>
     </BrowserRouter>
   );
 };
--- client/src/components/singlePost/SinglePost.tsx
+++ client/src/components/singlePost/SinglePost.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useParams } from 'react-router';
+import { Link } from 'react-router-dom';
 import {
   Box,
   Paper,
-  Link,
   Grid,
   Typography,
   Stack,
@@ -14,79 +15,77 @@ import { styled } from '@material-ui/core/styles';
 import EditIcon from '@material-ui/icons/Edit';
 import DeleteIcon from '@material-ui/icons/Delete';
 import { Image } from '../../utility/utilityStyled';
+import { postTypes } from '../../types/type';
+import { BlogsContext } from '../../context/blogsContext/BlogContext';
+
+type Props = {
+  posts: postTypes[];
+};
 
 const SinglePost: React.FC = () => {
+  const { posts } = useContext<Props>(BlogsContext);
+  const { postId } = useParams<any>();
+  const PublicFolder = 'http://localhost:3001/images/';
   return (
     <Paper sx={{ marginTop: '62px', borderRadius: '5px', marginRight: '20px' }}>
-      <Box sx={{ height: '500px' }}>
-        <Image src="https://source.unsplash.com/random" alt="" />
-      </Box>
+      {posts
+        ?.filter(post => post._id === postId)
+        .map(post => {
+          console.log('Post', post);
+          return (
+            <div key={post._id}>
+              {' '}
+              <Box sx={{ height: '500px' }}>
+                <Image src={PublicFolder + post.photo} alt="" />
+              </Box>
+              <Grid container p={2}>
+                <Grid item>
+                  <Stack direction="row">
+                    <Typography variant="h4" mr={8} gutterBottom>
+                      {post.title}
+                    </Typography>
+                    <Box sx={{ color: 'action.active' }}>
+                      <IconButton>
+                        <Badge>
+                          <EditIcon color="success" />
+                        </Badge>
+                      </IconButton>
+                      <IconButton>
+                        <Badge>
+                          <DeleteIcon sx={{ color: red[500] }} />
+                        </Badge>
+                      </IconButton>
+                    </Box>
+                  </Stack>
+                </Grid>
+                <Grid item mb={4}>
+                  <Stack
+                    sx={{
+                      flexDirection: 'row',
+                      justifyContent: 'space-between',
+                    }}
+                  >
+                    <Typography variant="subtitle1" color="text.secondary">
+                      Author:
+                      <Link to={`/?user=${post.username}`}>
+                        {post.username}
+                      </Link>
+                    </Typography>
+                    <Typography
+                      variant="subtitle1"
+                      color="text.secondary"
+                      gutterBottom
+                    >
+                      {new Date(post.createdAt).toDateString()}
+                    </Typography>
+                  </Stack>
 
-      <Grid container p={2}>
-        <Grid item>
-          <Stack direction="row">
-            <Typography variant="h4" mr={8} gutterBottom>
-              Text:Lorem ipsum dolor
-            </Typography>
-            <Box sx={{ color: 'action.active' }}>
-              <IconButton>
-                <Badge>
-                  <EditIcon color="success" />
-                </Badge>
-              </IconButton>
-              <IconButton>
-                <Badge>
-                  <DeleteIcon sx={{ color: red[500] }} />
-                </Badge>
-              </IconButton>
-            </Box>
-          </Stack>
-        </Grid>
-        <Grid item mb={4}>
-          <Stack sx={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-            <Typography variant="subtitle1" color="text.secondary">
-              Author:
-              <Link className="link" href="/posts?username=Safak">
-                Dipak
-              </Link>
-            </Typography>
-            <Typography variant="subtitle1" color="text.secondary" gutterBottom>
-              1 day ago
-            </Typography>
-          </Stack>
-
-          <ParagraphText>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste error
-            quibusdam ipsa quis quidem doloribus eos, dolore ea iusto impedit!
-            Voluptatum necessitatibus eum beatae, adipisci voluptas a odit modi
-            eos! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste
-            error quibusdam ipsa quis quidem doloribus eos, dolore ea iusto
-            impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas a
-            odit modi eos! Lorem, ipsum dolor sit amet consectetur adipisicing
-            elit. Iste error quibusdam ipsa quis quidem doloribus eos, dolore ea
-            iusto impedit! Voluptatum necessitatibus eum beatae, adipisci
-            voluptas a odit modi eos! Lorem, ipsum dolor sit amet consectetur
-            adipisicing elit. Iste error quibusdam ipsa quis quidem doloribus
-            eos, dolore ea iusto impedit! Voluptatum necessitatibus eum beatae,
-            adipisci voluptas a odit modi eos! Lorem, ipsum dolor sit amet
-            consectetur adipisicing elit. Iste error quibusdam ipsa quis quidem
-            doloribus eos, dolore ea iusto impedit! Voluptatum necessitatibus
-            eum beatae, adipisci voluptas a odit modi eos!
-            <br />
-            <br />
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste error
-            quibusdam ipsa quis quidem doloribus eos, dolore ea iusto impedit!
-            Voluptatum necessitatibus eum beatae, adipisci voluptas a odit modi
-            eos! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste
-            error quibusdam ipsa quis quidem doloribus eos, dolore ea iusto
-            impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas a
-            odit modi eos! Lorem, ipsum dolor sit amet consectetur adipisicing
-            elit. Iste error quibusdam ipsa quis quidem doloribus eos, dolore ea
-            iusto impedit! Voluptatum necessitatibus eum beatae, adipisci
-            voluptas a odit modi eos! Lorem, ipsum dolor sit amet consectetur.
-          </ParagraphText>
-        </Grid>
-      </Grid>
+                  <ParagraphText>{post.desc}</ParagraphText>
+                </Grid>
+              </Grid>
+            </div>
+          );
+        })}
     </Paper>
   );
 };
